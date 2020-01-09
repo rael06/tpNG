bd0b34ca88469b43f6ea6360aead7bdc02b94694
rael <rael06@hotmail.fr>
2020-01-09 23:13:22 +0100

    tp init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { TitleComponent } from './title/title.component';
+import { ZoomerComponent } from './zoomer/zoomer.component';
+import { SizeButtonComponent } from './size-button/size-button.component';
+import { InputFieldComponent } from './input-field/input-field.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TitleComponent,
+    ZoomerComponent,
+    SizeButtonComponent,
+    InputFieldComponent
   ],
   imports: [
     BrowserModule,
