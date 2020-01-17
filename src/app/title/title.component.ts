110822fa4b320101a8fb9ab7801663659de69989
rael <rael06@hotmail.fr>
2020-01-17 12:26:11 +0100

    tp angular after services

--- src/app/title/title.component.ts
+++ src/app/title/title.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnChanges, OnInit} from '@angular/core';
 import {AlertSizePipe} from '../alert-size.pipe';
+import {CheckService} from '../check.service';
 
 @Component({
     selector: 'app-title',
@@ -9,18 +10,24 @@ import {AlertSizePipe} from '../alert-size.pipe';
 export class TitleComponent implements OnInit, OnChanges {
     @Input() size: number;
     @Input() title: string;
+    validity: string;
     // private alert = 'Out of range';
     private message: string | number;
     private alertSize = false;
 
-    constructor(private alertSizePipe: AlertSizePipe) {
+    constructor(
+        private alertSizePipe: AlertSizePipe,
+        private checkSize: CheckService
+    ) {
     }
+
 
     ngOnInit() {
         this.title = 'Mon Titre';
     }
 
     ngOnChanges() {
+        this.validity = this.checkSize.isValid(this.size) ? 'valide' : 'invalide';
         this.alertSize = this.alertSizePipe.transform(this.size, 18, 22) === this.alertSizePipe.outOfRange;
         this.message = this.alertSize ?
             this.alertSizePipe.outOfRange :
